4d9e8bd05bf8dc3613512f5d0049b825eb740d13
cloudyunfan <yunfan2clouyf@gmail.com>
2016-11-22 15:30:16 +0800

    with effective time of channel predict

--- test.m
+++ test.m
@@ -1,6 +1,10 @@
 clc;
 clear all;
-con_pktloss = 0;
-request = 10/con_pktloss
+for lambda = 1 : 3
+for i = 1 : 3
+    slotNO_of_sp(i,:) = i*ones(1,3);
 
-
+end 
+    save(strcat([num2str(lambda), '.mat']), 'slotNO_of_sp');
+end
+load(num2str(1));
